6d5020dd2cbfc498af3c7da0664195eaac76c75c
c-ehrlich <ehrlich.christopher@gmail.com>
2024-12-31 16:50:06 +0100

    generation and playback with index + fallback

--- features/audio/sound-file.ts
+++ features/audio/sound-file.ts
@@ -1,9 +1,19 @@
+import { audioFiles } from "@/assets/audio/_audio-files";
 import { Audio } from "expo-av";
+import { speakText } from "./speak-text";
 
 export const preloadAudioFile = async (fileName: string) => {
   const sound = new Audio.Sound();
   try {
-    const file = require(`../../assets/audio/output_0_mp3.mp3`);
+    fileName = "การ"; // TODO: BEFORE MERGE - don't overwrite the filename
+
+    // @ts-expect-error we are intentionally indexing into something that might be undefined
+    const file = audioFiles[fileName];
+
+    if (!file) {
+      return;
+    }
+
     await sound.loadAsync(file);
 
     // we will never need the file that long
@@ -18,8 +28,16 @@ export const preloadAudioFile = async (fileName: string) => {
 export const playAudioFile = async (fileName: string) => {
   const sound = new Audio.Sound();
   try {
-    const file = require(`../../assets/audio/output_0_mp3.mp3`);
-    console.log("file", file);
+    fileName = "การ"; // TODO: BEFORE MERGE - don't overwrite the filename
+
+    // @ts-expect-error we are intentionally indexing into something that might be undefined
+    const file = audioFiles[fileName];
+
+    if (!file) {
+      speakText(fileName);
+      return;
+    }
+
     await sound.loadAsync(file);
     await sound.playAsync();
 
@@ -28,6 +46,6 @@ export const playAudioFile = async (fileName: string) => {
       sound.unloadAsync();
     }, 30_000);
   } catch (error) {
-    alert(`Error preloading sound: ${JSON.stringify(error)}`);
+    alert(`Error playing sound: ${JSON.stringify(error)}`);
   }
 };
