3ce68fccd0bd25d0b9536b4883e7fdc37919fdf0
c-ehrlich <ehrlich.christopher@gmail.com>
2025-01-06 21:56:35 +0100

    slightly less awful learn index layout

--- app/learn/index.tsx
+++ app/learn/index.tsx
@@ -1,21 +1,18 @@
 import { Heading } from "@/components/ui/heading";
 import { LEARN_CHAPTERS } from "@/features/learn/chapters";
 import { router } from "expo-router";
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, SafeAreaView, Text, TouchableOpacity } from "react-native";
 
 export default function LearnIndex() {
   return (
-    <View className="p-4 flex flex-col gap-2 min-h-screen-safe">
-      <Heading size="xl">Chapters</Heading>
+    <SafeAreaView>
       <FlatList
-        className="flex flex-col gap-2"
+        className="p-4"
         data={LEARN_CHAPTERS}
-        renderItem={({ item, index }) => (
+        renderItem={({ item }) => (
           // @ts-expect-error not sure why route types are off here
           <TouchableOpacity onPress={() => router.push(`/learn/${item.id}`)}>
-            <Heading size="md">
-              Chapter {index + 1}: {item.name}
-            </Heading>
+            <Heading size="md">{item.name}</Heading>
             {item.description ? (
               <FlatList
                 data={item.description}
@@ -25,6 +22,6 @@ export default function LearnIndex() {
           </TouchableOpacity>
         )}
       />
-    </View>
+    </SafeAreaView>
   );
 }
